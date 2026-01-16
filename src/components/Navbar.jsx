b1614664f4fcefd647b35d357a41a2c7be72f8b9
Long Vo <longvo.work84@gmail.com>
2026-01-16 00:56:04 -0600

    updated home button with smooth scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="flex justify-between items-center">
         <ul className="flex gap-8 ml-auto">
           <li>
-            <Link to="/" className="hover:text-primary-cream transition font-dm-sans text-sm font-bold">suture the future</Link>
+            <a href="#home" className="hover:text-primary-cream transition font-dm-sans text-sm font-bold">suture the future</a>
           </li>
           {/*<li>
             <Link to="/team" className="hover:text-primary-cream transition font-dm-sans text-sm font-bold">meet the team</Link>
